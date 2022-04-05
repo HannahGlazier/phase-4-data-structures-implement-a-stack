bb3b39de9a81db81d8a4c18b1802236eeaccdfac
Hannah Glazier <hrayglazier@aol.com>
2022-04-05 15:40:36 -0600

    commit tree 3b84795646cf021dca008dca1171f17d7c2e9350

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
-# your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+        
+    end
+
+    def push(value) 
+        @stack.length == limit ? raise( "Stack overflow") : @stack.push(value)
+        @stack.push(value) 
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+
+
+end
